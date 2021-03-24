2ffcd68c252b26c6dafb3c56e27a218235cb3ce8
pyrokiss <44851472+pyrokiss@users.noreply.github.com>
2021-03-25 00:12:34 +0200

    Add files via upload

--- dbis_lab1.py
+++ dbis_lab1.py
@@ -4,6 +4,7 @@ import csv
 import re
 import time
 import codecs
+import os
 
 from psycopg2 import sql
 from psycopg2._psycopg import OperationalError
@@ -13,7 +14,7 @@ file_name, dbname, user, password, host, port = sys.argv
 pattern_mark = re.compile(r'^\d+,\d+$')  # шаблон для поиска чисел с запятой в данных
 pattern_file = re.compile(r'\d{4}')  # шаблон для поиска года в названии файла
 
-data_file = 'Odata2020File.csv'  # название файла с данными
+data_files = ['Odata2020File.csv', 'Odata2019File.csv']   # название файла с данными
 counter_file = 'string_number.txt' # название файл с счетчиком
 
 
@@ -22,7 +23,7 @@ def connection_true(dbname, user, password, host, port):
     try:
         conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
     except:
-        print("Lol")
+        print("Connection error")
     return conn
 
 def execute_query(query, cursor, connection):
@@ -34,30 +35,37 @@ def execute_query(query, cursor, connection):
         connection.close()
         sys.exit()
 
-def execute_read_query(query, cursor):
-    result = None
+def execute_read_query(query, connection):
+    result = []
     try:
-        cursor.execute(query)
-        result = cursor.fetchall()
+        with connection.cursor() as cur:
+            cur.execute(query)
+            result = cur.fetchall()
         return result
     except OperationalError as e:
         print(f"The error '{e}' occurred")
+        connection.close()
+        sys.exit()
 
 def create_table(connection):
-    with open("string_number.txt", "w") as counter:
-        counter.write("0")
+    if not os.path.exists(counter_file):
+        with open(counter_file, "w") as counter:
+            counter.write("0")
     with open("create_table.sql", 'r') as create:
         res = ''.join([row for row in create])
         with connection.cursor() as cur:
             execute_query(res, cur)
         connection.commit()
 
-def database_task():
+def database_task(connection):
     with codecs.open("database_task.sql", 'r', encoding="utf-8") as task:
         res = ''.join([row for row in task])
-        result = execute_read_query(conn, res)
-        for row in result:
-            print(row)
+        result = execute_read_query(res, connection)
+        with open("query_result.csv", "w") as query_result:
+            for row in result:
+                query_result.write(",".join([str(elem) for elem in row])+"\n")
+        #for row in result:
+        #    print(row)
 
 conn = connection_true(dbname, user, password, host, port)
 if conn is None:
@@ -65,31 +73,44 @@ if conn is None:
 
 cursor = conn.cursor()
 create_table(conn)
+for data_file in data_files:
+    with open(data_file, newline='', encoding="cp1251") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        head = next(reader)
+        head.insert(0, "year")
+        data_year = re.findall(pattern_file, data_file)[0]
+        with open("string_number.txt", "r") as f1:
+            result = int(f1.read())
+            for n, row in enumerate(reader):
+                row.insert(0, data_year)
+                try:
+                    if n >= result:
+                        for b, i in enumerate(row):
+                            if re.match(pattern_mark, i):
+                                row[b] = row[b].replace(",", ".")
+                            if i == "null":
+                                row[b] = None
+                        insert = sql.SQL('INSERT INTO ZNO_DATA_TEST ({}) VALUES ({})').format(
+                            sql.SQL(',').join([sql.Identifier(i.lower()) for i in head]),
+                            sql.SQL(',').join(map(sql.Literal, row))
+                        )
+                        cursor.execute(insert)
+                        if (n+1) % 100 == 0 and n > 0:
+                            conn.commit()
+                            with open(counter_file, "w") as f:
+                               f.write(str(n+1))
 
-with open(data_file, newline='', encoding="cp1251") as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    head = next(reader)
-    head.insert(0, "year")
-    data_year = re.findall(pattern_file, data_file)[0]
-    with open("string_number.txt", "r") as f1:
-        result = int(f1.read())
-        for n, row in enumerate(reader):
-            row.insert(0, data_year)
-            if n >= result:
-                for b, i in enumerate(row):
-                    if re.match(pattern_mark, i):
-                        row[b] = row[b].replace(",", ".")
-                    if i == "null":
-                        row[b] = None
-                insert = sql.SQL('INSERT INTO ZNO_DATA_TEST ({}) VALUES ({})').format(
-                    sql.SQL(',').join([sql.Identifier(i.lower()) for i in head]),
-                    sql.SQL(',').join(map(sql.Literal, row))
-                )
-                cursor.execute(insert)
-                if (n+1) % 100 == 0 and n > 0:
-                    conn.commit()
-                    with open("string_number.txt", "w") as f:
-                        f.write(str(n+1))
-                        f.close()
-                print(n)
-                time.sleep(0.15)
+                        print(n)
+                        time.sleep(0.15)
+                except (psycopg2.ProgrammingError, psycopg2.IntegrityError, psycopg2.DataError) as e:
+                    conn.rollback()
+                    if conn is not None:
+                        conn.close()
+                    print(f"Inserts errors = {e}")
+                    sys.exit()
+                except (psycopg2.OperationalError, psycopg2.DatabaseError, psycopg2.InterfaceError) as e:
+                    print(f"Connections errors = {e}")
+                    sys.exit()
+os.remove(counter_file)
+database_task(conn)
+conn.close()
